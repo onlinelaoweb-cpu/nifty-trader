2aae20badb918ebc0e62ff3213208fa728c69011
OnlineLao Websolutions <onlinelaoweb@gmail.com>
2026-05-18 00:24:58 +0530

    Update websocket.js

--- src/api/websocket.js
+++ src/api/websocket.js
@@ -1,5 +1,7 @@
 const WebSocket = require('ws');
 
+let latestPrice = 0;
+
 function startWebSocket(jwtToken) {
 
     const ws = new WebSocket(
@@ -11,33 +13,60 @@ function startWebSocket(jwtToken) {
         console.log('WebSocket Connected');
 
         ws.send(JSON.stringify({
+
             correlationID: 'vardaannifty',
+
             action: 1,
+
             params: {
+
                 mode: 1,
+
                 tokenList: [
                     {
                         exchangeType: 1,
                         tokens: ['26000']
                     }
                 ]
+
             }
+
         }));
 
     });
 
     ws.on('message', (data) => {
 
-        console.log(
-            'Live Tick:',
-            data.toString()
-        );
+        try {
+
+            const tick = JSON.parse(data);
+
+            console.log('LIVE TICK:', tick);
+
+            latestPrice = tick.last_traded_price || 0;
+
+        }
+
+        catch(err){
+
+            console.log('Raw Tick:', data.toString());
+
+        }
 
     });
 
     ws.on('error', (err) => {
 
-        console.error(err);
+        console.error(
+            'WebSocket Error:',
+            err
+        );
+
+    });
+
+    ws.on('close', () => {
+
+        console.log('WebSocket Closed');
 
     });
 
