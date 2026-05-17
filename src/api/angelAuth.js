0640b4c42cf2121b5ad2b5e30f4b87397516ab1a
OnlineLao Websolutions <onlinelaoweb@gmail.com>
2026-05-17 23:33:59 +0530

    Add Angel API login functionality
    Implement login function for Angel API using axios.

--- src/api/angelAuth.js
+++ src/api/angelAuth.js
@@ -1,1 +1,42 @@
+const axios = require('axios');
 
+async function loginAngel() {
+
+    try {
+
+        console.log('Trying Angel Login...');
+
+        const response = await axios.post(
+            'https://apiconnect.angelone.in/rest/auth/angelbroking/user/v1/loginByPassword',
+            {
+                clientcode: process.env.ANGEL_CLIENT_ID,
+                password: process.env.ANGEL_PASSWORD,
+                totp: process.env.ANGEL_TOTP
+            },
+            {
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Accept': 'application/json',
+                    'X-PrivateKey': process.env.ANGEL_API_KEY
+                }
+            }
+        );
+
+        console.log('Angel Response:', response.data);
+
+        return response.data.data;
+
+    }
+
+    catch (err) {
+
+        console.error(
+            'FULL ANGEL ERROR:',
+            err.response?.data || err.message
+        );
+
+    }
+
+}
+
+module.exports = loginAngel;
