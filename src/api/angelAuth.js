e865a91937f74dd17e9181ad8ea35cb5b6964436
OnlineLao Websolutions <onlinelaoweb@gmail.com>
2026-05-17 23:51:11 +0530

    Fix formatting and update module export in angelAuth

--- src/api/angelAuth.js
+++ src/api/angelAuth.js
@@ -8,15 +8,19 @@ async function loginAngel() {
 
         const response = await axios.post(
             'https://apiconnect.angelone.in/rest/auth/angelbroking/user/v1/loginByPassword',
+
             {
                 clientcode: process.env.ANGEL_CLIENT_ID,
                 password: process.env.ANGEL_PASSWORD,
                 totp: process.env.ANGEL_TOTP
             },
+
             {
                 headers: {
                     'Content-Type': 'application/json',
                     'Accept': 'application/json',
+                    'X-UserType': 'USER',
+                    'X-SourceID': 'WEB',
                     'X-PrivateKey': process.env.ANGEL_API_KEY
                 }
             }
