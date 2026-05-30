44f27ae29d81deeaab1178e12d861376037c4bf1
OnlineLao Websolutions <onlinelaoweb@gmail.com>
2026-05-31 01:33:18 +0530

    cues

--- src/api/globalCues.js
+++ src/api/globalCues.js
@@ -84,7 +84,7 @@ async function fetchGlobalCues() {
         fetchQuote('USDINR=X'), fetchQuote('DX-Y.NYB'),
         fetchQuote('CL=F'), fetchQuote('BZ=F'), fetchQuote('GC=F'), fetchQuote('SI=F'),
         fetchQuote('^NSEBANK'), fetchQuote('^CNXIT'), fetchQuote('^CNXAUTO'), fetchQuote('^CNXMETAL'),
-        fetchQuote('NIFTY_FUT.NS').catch(() => fetchQuote('%5ENSEI')),  // GIFT Nifty futures proxy
+        fetchQuote('NIFTY_GIFT.NS').catch(() => fetchQuote('NIFTYBEES.NS')).catch(() => fetchQuote('%5ENSEI')),  // GIFT Nifty proxy
         bankNiftyVWAPLead(),
     ]);
 
